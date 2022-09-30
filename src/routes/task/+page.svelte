59fb83d6fdecf5ad8237ea6ff09454c72f69b2e5
mfranzon <franzonmarco94@gmail.com>
2022-09-30 16:55:11 +0200

    add task page and, list, creation and apply methods

---
+++ src/routes/task/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import TaskNew from "$lib/TaskNew.svelte"
+  import TaskList from "$lib/TaskList.svelte"
+  import { doLogout } from "$lib/login.svelte"
+  import "/src/global.css"
+
+  export let data;
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light" >
+  <a href="/me">Home</a>
+  <a class="left" href="/task">Tasks</a>
+<button class="btn btn-sm btn-primary" on:click={doLogout}>Logout</button>
+</nav>
+
+<TaskNew bind:taskList={ data.taskList }></TaskNew>
+<hr>
+<TaskList bind:taskList={ data.taskList }></TaskList>
