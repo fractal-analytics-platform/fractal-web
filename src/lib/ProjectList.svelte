52eacee3d98a263f0923be1dd68a6dfa96ff48a3
mfranzon <franzonmarco94@gmail.com>
2022-09-30 16:55:27 +0200

    add style

--- src/lib/ProjectList.svelte
+++ src/lib/ProjectList.svelte
@@ -15,19 +15,33 @@
 </script>
 
 {#if projectList}
-<h3>Project List to become </h3>
-  {#each projectList as p}
-    <div>
-    <a href='/me/project/{p.id}'>{p.id} - {p.name}</a>
-    <button on:click|preventDefault={doDelete(p)}>delete</button>
-    </div>
-  {/each}
+<h3 class='text-center' style="padding-top:5%;" >Project List</h3>
+  <div class="container mt-3">
+    <table class="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        {#each projectList as p}
+        <tr>
+          <th scope="row"> {p.id}</th>
+          <td><a href='/me/project/{p.id}'>{p.name}</a></td>
+          <td>
+            <div class="d-flex flex-row  mb-3">
+              <div ><button type="button" class="btn" on:click|preventDefault={doDelete(p)}>
+                <i class="material-icons text-danger">Delete</i>
+              </button></div>
+          </td>
+        </tr>
+        {/each}
+
+      </tbody>
+    </table>
+  </div>
 {/if}
 
-<style>
-  div {
-    padding: 1rem;
-    box-shadow: -1px 1px 11px 4px #898989;
-    margin: 20px;
-  }
-</style>
