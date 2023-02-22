bc4e8937f33713ec134c4b0741405675665406a7
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-02-22 10:55:19 +0100

    List projects within a table

--- src/lib/components/ProjectsList.svelte
+++ src/lib/components/ProjectsList.svelte
@@ -2,11 +2,25 @@
     export let projects = []
 </script>
 
-<h1>Projects list component</h1>
-<ul>
-    { #each projects as { id, name, project_dir } }
-        <li>
-            <span>{id}</span> - <span>{name}</span> - <span>{project_dir}</span>
-        </li>
-    {/each}
-</ul>
+<h1>Projects</h1>
+
+<table class="table table-hover">
+    <thead class="table-light">
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Directory</th>
+            <th>Options</th>
+        </tr>
+    </thead>
+    <tbody>
+        { #each projects as { id, name, project_dir } }
+            <tr>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{project_dir}</td>
+                <td></td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
