9b9b208dd6a27d127f9dcd7b86a6f2af8e20c12c
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-02-28 23:03:42 +0100

    Update tasks page layout

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -27,60 +27,85 @@
 	$: actionResult(form)
 </script>
 <h1>Tasks page</h1>
-<form method="post" action="?/create" class="" use:enhance>
-	<div class="row g-3">
-		<div class="col-6">
-			<div class="input-group">
-				<div class="input-group-text">Task name</div>
-				<input name="name" type="text" class="form-control">
-			</div>
-		</div>
-		<div class="col-12">
-			<div class="input-group">
-				<div class="input-group-text">Command</div>
-				<input name="command" type="text" class="form-control">
-			</div>
-		</div>
-		<div class="col-6">
-			<div class="input-group">
-				<div class="input-group-text">Source</div>
-				<input name="source" type="text" class="form-control">
-			</div>
-		</div>
-		<div class="row">
 
-		</div>
-		<div class="col-6">
-			<div class="input-group">
-				<div class="input-group-text">Input type</div>
-				<input name="input_type" type="text" class="form-control">
+
+<div class='container mt-4'>
+	<div class='row'>
+		<form method="post" action="?/create" class="" use:enhance>
+			<div class="row g-3">
+				<div class="col-6">
+					<div class="input-group">
+						<div class="input-group-text">Task name</div>
+						<input name="name" type="text" class="form-control">
+					</div>
+				</div>
+				<div class="col-12">
+					<div class="input-group">
+						<div class="input-group-text">Command</div>
+						<input name="command" type="text" class="form-control">
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="input-group">
+						<div class="input-group-text">Source</div>
+						<input name="source" type="text" class="form-control">
+					</div>
+				</div>
+				<div class="row">
+
+				</div>
+				<div class="col-6">
+					<div class="input-group">
+						<div class="input-group-text">Input type</div>
+						<input name="input_type" type="text" class="form-control">
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="input-group">
+						<div class="input-group-text">Output type</div>
+						<input name="output_type" type="text" class="form-control">
+					</div>
+				</div>
+				<div class="col-auto">
+					<button type="submit" class="btn btn-primary">Create</button>
+				</div>
 			</div>
-		</div>
-		<div class="col-6">
-			<div class="input-group">
-				<div class="input-group-text">Output type</div>
-				<input name="output_type" type="text" class="form-control">
+		</form>
+		{#if errorReasons != '' }
+			<div class="col-12 p-2">
+				<div class="alert alert-danger">
+					<pre>There has been an error, reason:</pre>
+					<pre>{errorReasons}</pre>
+				</div>
 			</div>
-		</div>
-		<div class="col-12">
-			<div class="input-group">
-				<div class="input-group-text">Output type</div>
-				<input name="output_type" type="text" class="form-control">
-			</div>
-		</div>
-		<div class="col-auto">
-			<button type="submit" class="btn btn-primary">Create</button>
+		{/if}
+	</div>
+	<div class='row mt-4'>
+		<div class='col-12'>
+			<table class='table'>
+				<thead class='table-light'>
+					<tr>
+						<th>#</th>
+						<th>Name</th>
+						<th>Command</th>
+						<th>Source</th>
+						<th>Input type</th>
+						<th>Output type</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each data.tasks as task}
+						<tr>
+							<td>{task.id}</td>
+							<td>{task.name}</td>
+							<td><code>{task.command}</code></td>
+							<td><code>{task.source}</code></td>
+							<td><pre>{task.input_type}</pre></td>
+							<td><pre>{task.output_type}</pre></td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
 		</div>
 	</div>
-</form>
-{#if errorReasons != '' }
-	<div class="row p-4">
-		<div class="alert alert-danger">
-			<pre>There has been an error, reason:</pre>
-			<pre>{errorReasons}</pre>
-		</div>
-	</div>
-{/if}
-{#each data.tasks as task}
-	<p>{task.name}</p>
-{/each}
+</div>
