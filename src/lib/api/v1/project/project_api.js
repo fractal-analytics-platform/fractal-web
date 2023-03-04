b386046a920e84438ce63e3cd5311af6c45f277c
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-03-04 10:18:22 +0100

    Refactor projects page
    - Refactor list projects logic

--- src/lib/api/v1/project/project_api.js
+++ src/lib/api/v1/project/project_api.js
@@ -1,20 +1,16 @@
 import { PUBLIC_FRACTAL_SERVER_HOST } from '$env/static/public'
 
-export async function list_projects(fetch, cookies) {
+export async function listProjects() {
 
-  // Set headers
-  const headers = new Headers()
-  headers.append('Authorization', cookies.get('AccessToken'))
-
-  // Compose request
   const response = await fetch(PUBLIC_FRACTAL_SERVER_HOST + '/api/v1/project', {
     method: 'GET',
-    headers: headers
+    credentials: 'include'
   })
-    // Handle response
-    .then(response => {
-      return response.json()
-    })
 
-  return response
+  if (response.ok) {
+    return await response.json()
+  }
+
+  console.error('Client unable to fetch projects list')
+  throw new Error('Unable to list projects')
 }
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,9 +1,19 @@
 <script>
-    import ProjectsList from '$lib/components/ProjectsList.svelte'
-    import ProjectInfoModal from '$lib/components/ProjectInfoModal.svelte'
- 	  export let data
+  import { onMount } from 'svelte'
+  import { listProjects } from '$lib/api/v1/project/project_api'
+  import ProjectsList from '$lib/components/ProjectsList.svelte'
+  import ProjectInfoModal from '$lib/components/ProjectInfoModal.svelte'
+
+  let projects = []
+
+  onMount(async () => {
+    const projectList = await listProjects().catch(() => {
+      return []
+    })
+    projects = projectList
+  })
+
 </script>
 
-<ProjectsList bind:projects={data.projects} />
-
 <ProjectInfoModal></ProjectInfoModal>
+<ProjectsList bind:projects />
