b11eb32db85dcf065849fd62159c3bb59eaa64f4
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-03-09 10:18:04 +0100

    Workflow list shows id, name

--- src/lib/components/projects/WorkflowsList.svelte
+++ src/lib/components/projects/WorkflowsList.svelte
@@ -9,13 +9,17 @@
   <table class="table align-middle">
     <thead class="table-light">
       <tr>
-        <th>Id</th>
+        <th class="col-4">Id</th>
+        <th class="col-4">Name</th>
+        <th>Options</th>
       </tr>
     </thead>
     <tbody>
-      {#each workflows as { id }}
+      {#each workflows as { id, name }}
         <tr>
           <td>{id}</td>
+          <td>{name}</td>
+          <td></td>
         </tr>
       {/each}
     </tbody>
