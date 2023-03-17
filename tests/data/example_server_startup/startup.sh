48c45c51a3ee392551d09c4ee6e1add7c24de8e8
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-03-17 10:10:43 +0100

    Add tests/data/example_server_startup

---
+++ tests/data/example_server_startup/startup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+
+echo -e "\
+DEPLOYMENT_TYPE=testing
+JWT_SECRET_KEY=secret
+SQLITE_PATH=`pwd`/test.db
+FRACTAL_TASKS_DIR=`pwd`/FRACTAL_TASKS_DIR
+FRACTAL_LOGGING_LEVEL=20
+FRACTAL_RUNNER_BACKEND=local
+FRACTAL_RUNNER_WORKING_BASE_DIR=`pwd`/artifacts
+FRACTAL_ADMIN_DEFAULT_EMAIL=admin@fractal.xy
+FRACTAL_ADMIN_DEFAULT_PASSWORD=1234
+JWT_EXPIRE_SECONDS=84600
+" > .fractal_server.env
+
+
+
+rm test.db
+rm -r FRACTAL_TASKS_DIR
+
+# Create an empty db
+fractalctl set-db
+
+# Start the server
+fractalctl start --port 8000
