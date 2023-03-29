571db0b5c8642353be63cb3854099301315f127f
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-03-29 11:03:40 +0200

    Wiring entry client updates with server

--- src/lib/components/workflow/MetaPropertiesForm.svelte
+++ src/lib/components/workflow/MetaPropertiesForm.svelte
@@ -6,22 +6,30 @@
   //
   // This component also manages the overall form structure of meta properties.
   // The form should be structured in multiple levels of depth, and support complex structure.
+  import { updateWorkflowTaskMetadata } from '$lib/api/v1/workflow/workflow_api'
   import FormBuilder from '$lib/components/workflow/common/FormBuilder.svelte'
 
+  // Workflow id
+  export let workflowId
+  // Workflow task id
+  export let taskId
+  // Workflow task meta properties
   export let metaProperties = {}
 
-  $: {
-    console.log(metaProperties)
-  }
 
-  function handlePropertyUpdate(event) {
-    console.log(event.detail)
-    console.log(metaProperties)
-    // metaProperties[event.detail.name] = event.detail.value
+  async function handleEntryUpdate(updatedEntry) {
+    await updateWorkflowTaskMetadata(workflowId, taskId, updatedEntry)
+      .then((response) => {
+        metaProperties = response.meta
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
 </script>
 
 <div>
-  <FormBuilder entry={metaProperties} on:entryUpdated={handlePropertyUpdate}></FormBuilder>
+  <FormBuilder entry={metaProperties} updateEntry={handleEntryUpdate}></FormBuilder>
 </div>
