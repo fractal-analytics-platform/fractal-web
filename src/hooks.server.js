7340b37fd5230189884409e0c2b7d11eac8f5c39
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-05-11 23:49:57 +0200

    Authentication guard
    - If an authenticated request to /auth/whoami responds with a bad status code, then redirect user to login page.
    - Prevent guarding on /; /auth*

---
+++ src/hooks.server.js
@@ -0,0 +1,23 @@
+import { FRACTAL_SERVER_HOST } from '$env/static/private'
+
+export async function handle({ event, resolve }) {
+
+	if (event.url.pathname == '/' || event.url.pathname.startsWith('/auth')) {
+		return await resolve(event)
+	}
+
+	// Authentication guard
+	if (event.cookies) {
+		// const token = cookie
+		const whoami = await event.fetch(`${FRACTAL_SERVER_HOST}/auth/whoami`)
+		if (whoami.ok) {
+			console.log('Authorized')
+		} else {
+			console.log('Not authorized')
+			return new Response(null, { status: 302, headers: { location: '/auth/login' } })
+		}
+	} else {
+		console.log('Not authorized')
+		return new Response(null, { status: 302, headers: { location: '/auth/login' } })
+	}
+}
