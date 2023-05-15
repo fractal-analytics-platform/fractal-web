cd9ddba326e2d1fe3379f4787fad43f29fa2f411
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-05-15 14:58:29 +0200

    Update hooks logs to improve traceability experience

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,20 +1,25 @@
 import { FRACTAL_SERVER_HOST } from '$env/static/private'
 
 export async function handle({ event, resolve }) {
+	console.log(`[${event.request.method}] - ${event.url.pathname}`)
 
 	if (event.url.pathname == '/' || event.url.pathname.startsWith('/auth')) {
+		console.log('Public page - No auth required')
 		return await resolve(event)
 	}
 
 	// Authentication guard
 	const fastApiUsersAuth = event.cookies.get('fastapiusersauth')
 	if (!fastApiUsersAuth) {
+		console.log('Authentication required - No auth cookie found - Redirecting to login')
 		return new Response(null, { status: 302, headers: { location: '/auth/login' } })
 	}
+
 	const whoami = await event.fetch(`${FRACTAL_SERVER_HOST}/auth/whoami`)
 	if (whoami.ok) {
 		return await resolve(event)
 	} else {
+		console.log('Validation of authentication - Error loading user info')
 		return new Response(null, { status: 302, headers: { location: '/auth/login' } })
 	}
 }
