88537546f92c0800459ebde1bd4a3480cf43ef0b
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-05-16 18:07:14 +0200

    Update monitoring_api module with docs

--- src/lib/server/api/v1/monitoring_api.js
+++ src/lib/server/api/v1/monitoring_api.js
@@ -1,5 +1,12 @@
 import { FRACTAL_SERVER_HOST } from '$env/static/private'
 
+/**
+ * Fetches the list of jobs from the server
+ * @param fetch
+ * @param projectId
+ * @param jobId
+ * @returns {Promise<*>}
+ */
 export async function getJob(fetch, projectId, jobId) {
   console.log('Fetching job from server')
 
