f662fb8721b7db9f1958d17ba508a4a50298967f
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-05-16 18:14:43 +0200

    Update auth_api module with docs

--- src/lib/server/api/v1/auth_api.js
+++ src/lib/server/api/v1/auth_api.js
@@ -1,5 +1,11 @@
 import { FRACTAL_SERVER_HOST } from '$env/static/private'
 
+/**
+ * Request to authenticate user
+ * @param fetch
+ * @param data
+ * @returns {Promise<*>}
+ */
 export async function userAuthentication(fetch, data) {
 
   const response = await fetch(FRACTAL_SERVER_HOST + '/auth/token/login', {
@@ -16,6 +22,11 @@ export async function userAuthentication(fetch, data) {
   return await response.json()
 }
 
+/**
+ * Fetches user identity
+ * @param fetch
+ * @returns {Promise<*>}
+ */
 export async function whoami(fetch) {
 
   const response = await fetch(FRACTAL_SERVER_HOST + '/auth/whoami', {
@@ -31,6 +42,11 @@ export async function whoami(fetch) {
   throw new Error('Unable to fetch user identity')
 }
 
+/**
+ * Requests to close a user session on the server
+ * @param fetch
+ * @returns {Promise<void>}
+ */
 export async function logout(fetch) {
 
   const response = await fetch(FRACTAL_SERVER_HOST + '/auth/token/logout', {
