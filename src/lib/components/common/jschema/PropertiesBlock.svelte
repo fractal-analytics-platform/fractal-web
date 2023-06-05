03ea0f2748a64094d2bb0380bbaf3a6ccb692d1a
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-06-05 16:50:56 +0200

    PropertiesBlock structure component
    Responsible to render schema properties

---
+++ src/lib/components/common/jschema/PropertiesBlock.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { onMount, getContext } from 'svelte';
+	import PropertyDiscriminator from '$lib/components/common/jschema/PropertyDiscriminator.svelte';
+
+	export let properties = undefined;
+	export let blockKey = undefined;
+	let parsedProperties = undefined;
+
+	const context = getContext('jsonSchema');
+
+	console.log(context.getSchema());
+
+	onMount(() => {
+		// Make properties object into an array
+		parsedProperties = Object.keys(properties).map(key => {
+			const props = { ...properties[key] };
+			// If blockKey is undefined, set it to the key
+			if (blockKey === undefined) props.key = key;
+			// If blockKey is defined, set it to the blockKey
+			else props.key = blockKey + '.' + key;
+			// If props value is undefined then set it to the value
+			if (!props.value) props.value = context.getValue(props.key);
+			return props;
+		});
+		console.log('Parsed properties', parsedProperties);
+	});
+
+
+</script>
+
+<div class='properties-block'>
+  {#if parsedProperties}
+    {#each parsedProperties as prop}
+      <PropertyDiscriminator propertyData={prop}></PropertyDiscriminator>
+    {/each}
+  {/if}
+</div>
