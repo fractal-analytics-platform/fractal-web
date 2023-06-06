8878e8521468d8579a18e0f5f6c09cae2d8c0aca
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-06-06 16:25:20 +0200

    Minor fixes in JSchema component

--- src/lib/components/common/jschema/JSchema.svelte
+++ src/lib/components/common/jschema/JSchema.svelte
@@ -2,7 +2,7 @@
 	import { onMount, setContext } from 'svelte';
 	import PropertiesBlock from '$lib/components/common/jschema/PropertiesBlock.svelte';
 
-	let valid = false;
+	let valid = undefined;
 	let validatedSchema = undefined;
 	let data = undefined;
 
@@ -22,10 +22,13 @@
 	});
 
 	onMount(() => {
-		// Load schema and data from server
+		// The following is just an example to set a test page
+		// Load a default schema
 		let schema = '{"title": "TaskArguments", "type": "object", "properties": {"a": {"title": "A", "description": "This is the description of argument a", "default": 0, "type": "integer"}, "b": {"title": "B", "type": "string"}, "c": {"title": "C", "description": "A boolean field", "default": true, "type": "boolean"}, "d": {"title": "D", "description": "A list of numbers", "default": [0, 1, 2], "type": "array", "items": {"type": "integer"}}, "e": {"title": "E", "description": "A list of strings", "default": ["hello", "this", "test"], "type": "array", "items": {"type": "string"}}, "f": {"title": "F", "description": "A list of bools", "default": [true, false, false], "type": "array", "items": {"type": "boolean"}}, "g": {"title": "G", "description": "A nested list of integers", "default": [[1, 2], [3, 4], [5], [6]], "type": "array", "items": {"type": "array", "items": {"type": "integer"}}}, "h": {"title": "H", "description": "A nested list of strings", "default": [["this", "is"], ["a", "list"], ["of"], ["strings"]], "type": "array", "items": {"type": "array", "items": {"type": "string"}}}, "i": {"title": "I", "description": "A nested list of bools", "type": "array", "items": {"type": "array", "items": {"type": "boolean"}}}, "l": {"title": "L", "description": "An infinite nesting of lists", "default": [[[0]]], "type": "array", "items": {"type": "array", "items": {"type": "array", "items": {"type": "integer"}}}}, "m": {"title": "M", "description": "An infinite nesting of lists", "default": [[[0]]], "type": "array", "items": {"type": "array", "items": {"type": "array", "items": {"$ref": "#/definitions/Argument"}}}}, "n": {"title": "N", "type": "array", "items": {"type": "array", "items": {"type": "array", "items": {"$ref": "#/definitions/Argument"}}}}, "obj1": {"$ref": "#/definitions/Argument"}, "obj2": {"$ref": "#/definitions/Argument"}, "obj3": {"title": "Obj3", "description": "A custom object schema", "allOf": [{"$ref": "#/definitions/Argument"}]}, "obj4": {"title": "Obj4", "description": "A list of arguments", "default": [], "type": "array", "items": {"$ref": "#/definitions/Argument"}}}, "required": ["i", "n", "obj2", "obj3"], "definitions": {"SubArgument": {"title": "SubArgument", "type": "object", "properties": {"subA": {"title": "Suba", "description": "A sub argument property", "default": 0, "type": "integer"}}}, "Argument": {"title": "Argument", "type": "object", "properties": {"a": {"title": "A", "description": "A integer property of an object", "default": 3, "type": "integer"}, "b": {"title": "B", "description": "A string property of an object", "default": "hello", "type": "string"}, "c": {"title": "C", "type": "boolean"}, "d": {"title": "D", "default": [1, 2, 3], "type": "array", "items": {"type": "integer"}}, "e": {"$ref": "#/definitions/SubArgument"}}, "required": ["c", "e"]}}}';
 		validatedSchema = JSON.parse(schema);
 		valid = true;
+
+		// Load schema and data from server
 		// Validate schema
 		if (valid) {
 			// Create component structures
@@ -37,15 +40,17 @@
 
 	function handleJsonSchemaInput(event) {
 		// Get the value of the input
-		const value = event.target.value;
+		let value = event.target.value;
+
 		// Try to parse the value as JSON
 		try {
-			const parsedSchema = JSON.parse(value);
+			let parsedSchema = JSON.parse(value);
 			// If the parsed schema is valid, set the validated schema
 			validatedSchema = parsedSchema;
 			valid = true;
 		} catch (e) {
 			// If the parsed schema is invalid, set the validated schema to undefined
+			console.error('Unable to parse json schema', e);
 			validatedSchema = undefined;
 			valid = false;
 		}
@@ -87,13 +92,19 @@
 {#if valid && validatedSchema !== undefined}
 
   <!-- Start rendering the schema structure -->
-  {#key data}
-    <div id='json-schema'>
+  <div id='json-schema'>
+    {#key data}
       <PropertiesBlock
         properties={validatedSchema.properties}
       />
-    </div>
-  {/key}
+    {/key}
+  </div>
+
+{:else if valid === undefined}
+
+  <div>
+    <p>Loading schema</p>
+  </div>
 
 {:else}
 
@@ -101,6 +112,5 @@
     <p>Invalid schema or undefined parsed schema</p>
     <p>Something is wrong</p>
   </div>
-
 
 {/if}
