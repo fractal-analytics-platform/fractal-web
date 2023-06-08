a8c2bccfcb5d7761a3c4607f18996579203274f8
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-06-08 12:17:18 +0200

    New SchemaInput helper component to improve sandbox experience

--- src/lib/components/common/jschema/JSchema.svelte
+++ src/lib/components/common/jschema/JSchema.svelte
@@ -78,12 +78,14 @@
 	});
 
 	onMount(() => {
-		// The following is just an example to set a test page
 		// Load a default schema
-		let schema = '{"title": "TaskArguments", "type": "object", "properties": {"a": {"title": "A", "description": "This is the description of argument a", "default": 0, "type": "integer"}, "b": {"title": "B", "type": "string"}, "c": {"title": "C", "description": "A boolean field", "default": true, "type": "boolean"}, "d": {"title": "D", "description": "A list of numbers", "default": [0, 1, 2], "type": "array", "items": {"type": "integer"}}, "e": {"title": "E", "description": "A list of strings", "default": ["hello", "this", "test"], "type": "array", "items": {"type": "string"}}, "f": {"title": "F", "description": "A list of bools", "default": [true, false, false], "type": "array", "items": {"type": "boolean"}}, "g": {"title": "G", "description": "A nested list of integers", "default": [[1, 2], [3, 4], [5], [6]], "type": "array", "items": {"type": "array", "items": {"type": "integer"}}}, "h": {"title": "H", "description": "A nested list of strings", "default": [["this", "is"], ["a", "list"], ["of"], ["strings"]], "type": "array", "items": {"type": "array", "items": {"type": "string"}}}, "i": {"title": "I", "description": "A nested list of bools", "type": "array", "items": {"type": "array", "items": {"type": "boolean"}}}, "l": {"title": "L", "description": "An infinite nesting of lists", "default": [[[0]]], "type": "array", "items": {"type": "array", "items": {"type": "array", "items": {"type": "integer"}}}}, "m": {"title": "M", "description": "An infinite nesting of lists", "default": [[[0]]], "type": "array", "items": {"type": "array", "items": {"type": "array", "items": {"$ref": "#/definitions/Argument"}}}}, "n": {"title": "N", "type": "array", "items": {"type": "array", "items": {"type": "array", "items": {"$ref": "#/definitions/Argument"}}}}, "obj1": {"$ref": "#/definitions/Argument"}, "obj2": {"$ref": "#/definitions/Argument"}, "obj3": {"title": "Obj3", "description": "A custom object schema", "allOf": [{"$ref": "#/definitions/Argument"}]}, "obj4": {"title": "Obj4", "description": "A list of arguments", "default": [], "type": "array", "items": {"$ref": "#/definitions/Argument"}}}, "required": ["i", "n", "obj2", "obj3"], "definitions": {"SubArgument": {"title": "SubArgument", "type": "object", "properties": {"subA": {"title": "Suba", "description": "A sub argument property", "default": 0, "type": "integer"}}}, "Argument": {"title": "Argument", "type": "object", "properties": {"a": {"title": "A", "description": "A integer property of an object", "default": 3, "type": "integer"}, "b": {"title": "B", "description": "A string property of an object", "default": "hello", "type": "string"}, "c": {"title": "C", "type": "boolean"}, "d": {"title": "D", "default": [1, 2, 3], "type": "array", "items": {"type": "integer"}}, "e": {"$ref": "#/definitions/SubArgument"}}, "required": ["c", "e"]}}}';
-		validatedSchema = JSON.parse(schema);
-		valid = true;
+		if (schema !== undefined) {
+			validatedSchema = schema;
+		}
 
+		if (schemaData !== undefined) {
+			data = schemaData;
+		}
 		// Load schema and data from server
 		// Validate schema
 		if (valid) {
@@ -91,75 +93,38 @@
 			// Setup svelte context
 			// Add event listeners
 		}
-
 	});
 
-	function handleJsonSchemaInput(event) {
-		// Get the value of the input
-		let value = event.target.value;
-
-		// Try to parse the value as JSON
-		try {
-			let parsedSchema = JSON.parse(value);
-			// If the parsed schema is valid, set the validated schema
-			validatedSchema = parsedSchema;
-			valid = true;
-		} catch (e) {
-			// If the parsed schema is invalid, set the validated schema to undefined
-			console.error('Unable to parse json schema', e);
-			validatedSchema = undefined;
-			valid = false;
+	$: {
+		if (schema !== undefined) {
+			validatedSchema = schema;
+			isSchemaValid = validator.loadSchema(validatedSchema);
+			console.log('Validator loaded schema', validator.getErrors());
 		}
 	}
-
-	function handleJsonDataInput(event) {
-		// Get the value of the input
-		const value = event.target.value;
-		// Try to parse the value as JSON
-		if (value === '') {
-			data = {};
-			return;
-		}
-		try {
-			const parsedData = JSON.parse(value);
-			// If the parsed schema is valid, set the validated schema
-			data = parsedData;
-		} catch (e) {
-			// If the parsed schema is invalid, set the validated schema to undefined
-			data = undefined;
-			valid = false;
-		}
-	}
-
 
 </script>
 
 <div>
-  <h1>JSON Schema</h1>
-  <p>This is a test page for the JSON Schema component</p>
-
-  <div>
-    <input class='form-control' type='text' placeholder='JSON Schema as JSON string' on:change={handleJsonSchemaInput}>
-    <input class='form-control' type='text' placeholder='Default JSON values as JSON string'
-           on:change={handleJsonDataInput}>
-    <button class='btn btn-success {unsavedChanges ? "" : "disabled"}' on:click={() => {
-			console.log(data)
-			console.log(JSON.stringify(data))
-    }}>Save changes
-    </button>
-  </div>
-</div>
-
-<div>
-  <p>Status</p>
-  <p>Unsaved changes: {unsavedChanges}</p>
+  <p>Component status</p>
+  <ul>
+    <li>Unsaved changes: {unsavedChanges}</li>
+    <li>
+      <button class='btn btn-success {unsavedChanges ? "" : "disabled"}' on:click={() => {
+        console.log(data)
+        console.log(JSON.stringify(data))
+      }}>
+        Save changes
+      </button>
+    </li>
+  </ul>
 </div>
 
 {#if valid && validatedSchema !== undefined}
 
   <!-- Start rendering the schema structure -->
   <div id='json-schema'>
-    {#key data}
+    {#key validatedSchema}
       <PropertiesBlock
         properties={validatedSchema.properties}
       />
