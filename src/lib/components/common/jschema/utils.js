dc17ca62e14429431e3baa39c2298081016cdfa9
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-06-12 18:52:15 +0200

    Move the definition of context actions for schema management within a SchemaManager class

--- src/lib/components/common/jschema/utils.js
+++ src/lib/components/common/jschema/utils.js
@@ -32,3 +32,79 @@ export function resolveSchemaReference(reference, schema) {
 		}
 	}
 }
+
+
+export class SchemaManager {
+	keySeparator = '###';
+
+	constructor(schema, schemaData) {
+		this.loadSchema(schema);
+		this.loadSchemaData(schemaData);
+	}
+
+	loadSchema(schema) {
+		if (schema === undefined) {
+			throw new Error('Schema is undefined');
+		}
+		if (typeof schema === 'string') {
+			this.schema = JSON.parse(schema);
+		} else if (typeof schema === 'object') {
+			this.schema = schema;
+		}
+	}
+
+	loadSchemaData(schemaData) {
+		// Should check that schemaData is not undefined
+		if (schemaData === undefined) {
+			throw new Error('Schema data is undefined');
+		}
+		this.data = schemaData;
+	}
+
+	getValue(key) {
+		const keys = key.split(this.keySeparator);
+		let value = this.data;
+		keys.forEach(k => {
+			// Unless value is undefined, set the value to value[k]
+			if (value !== undefined) {
+				value = value[k];
+			}
+		});
+		return value;
+	}
+
+	updateValue(key, value) {
+		// Split key into keys
+		const keys = key.split(this.keySeparator);
+		// Get the last key
+		const lastKey = keys.pop();
+		// Get the object at the key
+		let object = this.data;
+		keys.forEach(k => {
+			// Unless k is #.#, set object to object[k]
+			if (k !== this.keySeparator)
+				object = object[k];
+		});
+		// Set the value at the last key
+		object[lastKey] = value;
+		// unsavedChanges = true;
+	}
+
+	setDefaultValue(key, value) {
+		console.log('setDefaultValue', key, value);
+		console.log(this.data);
+		// Split key into keys
+		const keys = key.split(this.keySeparator);
+		// Get the last key
+		const lastKey = keys.pop();
+		// Get the object at the key
+		let dataProperty = this.data;
+		keys.forEach(k => {
+			// Unless k is #.#, set object to object[k]
+			if (k !== this.keySeparator)
+				dataProperty = dataProperty[k];
+		});
+		// Set the value at the last key
+		dataProperty[lastKey] = value;
+	}
+}
