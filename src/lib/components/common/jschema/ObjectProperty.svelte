25b8f7c4a95fa240b8b84b7bce57e74b4de246d5
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-06-23 11:33:51 +0200

    First draft of refactoring for ObjectProperty

--- src/lib/components/common/jschema/ObjectProperty.svelte
+++ src/lib/components/common/jschema/ObjectProperty.svelte
@@ -6,6 +6,9 @@
 	let customObjectPropertyKey = undefined;
 	let addNestedObjectError = undefined;
 
+	let accordionParentKey = objectSchema.key.replaceAll('#', '');
+	let collapseSymbol = accordionParentKey + '-collapse';
+
 	function addNestedObjectProperty() {
 		try {
 			objectSchema.addProperty(customObjectPropertyKey);
@@ -20,34 +23,69 @@
 </script>
 
 {#if objectSchema}
-  <div style='background-color: #9e9e9e'>
-    <p>Object property</p>
 
-    {#if objectSchema.hasCustomKeyValues}
-      <p>Custom key values</p>
-      <form action=''>
-        <input type='text' bind:value={customObjectPropertyKey} placeholder='Key'>
-        <button on:click={addNestedObjectProperty}>Add property</button>
-      </form>
-      {#if objectSchema.properties}
-        {#key objectSchema.properties }
-          <PropertiesBlock blockKey={objectSchema.key} properties={objectSchema.properties} removePropertyBlock={(propertyKey) => {
-						propertyKey = propertyKey.split(objectSchema.manager.keySeparator).pop();
-						objectSchema.removeProperty(propertyKey);
-            objectSchema = objectSchema;
-          }} />
-        {/key}
-      {/if}
-    {:else}
-      {#if objectSchema.properties}
-        {#key objectSchema.properties }
-          <PropertiesBlock blockKey={objectSchema.key} properties={objectSchema.properties} />
-        {/key}
-      {/if}
-    {/if}
+  <div style='background-color: #9e9e9e' class='d-flex flex-column p-2'>
+    <div class='property-metadata d-flex flex-column w-50'>
+      <span class='fs-4 {objectSchema.isRequired() ? "fw-bold" : ""}'>{ objectSchema.title }</span>
+      <span>{ objectSchema.description }</span>
+    </div>
+    <div class='object-properties my-2'>
+      <div class='accordion' id='{accordionParentKey}'>
+        <div class='accordion-item'>
+          <div class='accordion-header'>
+            <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse'
+                    data-bs-target='#{collapseSymbol}'>Properties
+            </button>
+          </div>
+          <div id='{collapseSymbol}' class='accordion-collapse collapse' data-bs-parent='#{accordionParentKey}'>
+            <div class='accordion-body'>
+              {#if objectSchema.properties}
+                {#key objectSchema.properties }
+                  <PropertiesBlock blockKey={objectSchema.key} properties={objectSchema.properties} />
+                {/key}
+              {/if}
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 
 {:else}
+
+
+  <!--
+  {#if objectSchema}
+    <div style='background-color: #9e9e9e'>
+      <p>Object property</p>
+
+      {#if objectSchema.hasCustomKeyValues}
+        <p>Custom key values</p>
+        <form action=''>
+          <input type='text' bind:value={customObjectPropertyKey} placeholder='Key'>
+          <button on:click={addNestedObjectProperty}>Add property</button>
+        </form>
+        {#if objectSchema.properties}
+          {#key objectSchema.properties }
+            <PropertiesBlock blockKey={objectSchema.key} properties={objectSchema.properties} removePropertyBlock={(propertyKey) => {
+              propertyKey = propertyKey.split(objectSchema.manager.keySeparator).pop();
+              objectSchema.removeProperty(propertyKey);
+              objectSchema = objectSchema;
+            }} />
+          {/key}
+        {/if}
+      {:else}
+        {#if objectSchema.properties}
+          {#key objectSchema.properties }
+            <PropertiesBlock blockKey={objectSchema.key} properties={objectSchema.properties} />
+          {/key}
+        {/if}
+      {/if}
+    </div>
+
+  {:else}
+  -->
   <div>
     <p>Object schema is undefined</p>
   </div>
