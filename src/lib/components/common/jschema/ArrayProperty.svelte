5a884f08fd7900dd613a159563321aa55a16b98c
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-06-23 12:16:49 +0200

    Rebase ArrayProperty layout

--- src/lib/components/common/jschema/ArrayProperty.svelte
+++ src/lib/components/common/jschema/ArrayProperty.svelte
@@ -26,18 +26,24 @@
 </script>
 
 {#if schemaProperty }
-
-  <div style='background-color: rosybrown'>
-    <p>Array property</p>
-    <button class='btn btn-primary' on:click={addNestedProperty}>Add</button>
-
-    {#each nestedProperties as nestedProperty, index (nestedProperty.key)}
-      <div>
-        <button class='btn btn-warning' on:click={removeNestedProperty(index)}>Remove</button>
-        <PropertyDiscriminator schemaProperty={nestedProperty} />
+  <div style='background-color: rosybrown' class='d-flex flex-column p-2'>
+    <div class='property-metadata d-flex flex-column w-50'>
+      <span class='fs-4 {schemaProperty.isRequired() ? "fw-bold" : ""}'>{ schemaProperty.title }</span>
+      <span>{ schemaProperty.description }</span>
+    </div>
+    <div class='array-items my-2'>
+      <div class='d-flex justify-content-center'>
+        <button class='btn btn-primary' on:click={addNestedProperty}>Add argument to list</button>
       </div>
-    {/each}
-
+      <div>
+        {#each nestedProperties as nestedProperty, index (nestedProperty.key)}
+          <div>
+            <button class='btn btn-warning' on:click={removeNestedProperty(index)}>Remove</button>
+            <PropertyDiscriminator schemaProperty={nestedProperty} />
+          </div>
+        {/each}
+      </div>
+    </div>
   </div>
 
 {:else}
