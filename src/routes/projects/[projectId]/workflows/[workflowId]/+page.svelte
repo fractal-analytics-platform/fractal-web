4640779af19b64fdb2be7d8588951ea44e231ceb
Riccardo Pasianotto <pasianotto.engineering@gmail.com>
2023-06-26 14:13:43 +0200

    Decrease heading dimension for workflow page

--- src/routes/projects/[projectId]/workflows/[workflowId]/+page.svelte
+++ src/routes/projects/[projectId]/workflows/[workflowId]/+page.svelte
@@ -312,7 +312,7 @@
 {#if workflow}
 	<div class="container">
 		<div class="d-flex justify-content-between align-items-center my-3">
-			<h1>Workflow {workflow.name} #{$page.params.workflowId}</h1>
+			<h1 class='fs-3'>Workflow {workflow.name} #{$page.params.workflowId}</h1>
 		</div>
 		<div class="row">
 			<div class="col-4">
