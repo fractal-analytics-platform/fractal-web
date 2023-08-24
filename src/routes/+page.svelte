fd5bacf6d4fbf7d574d55fa2744f5f7cbcc8ffba
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-08-24 08:59:58 +0200

    Skeleton of new home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,25 @@
 <script>
 </script>
 
-<h1>Welcome to Fractal's web client.</h1>
+<h1>Welcome to Fractal web client.</h1>
+
+<div>
+IN PROGRESS
+<br>
+Welcome to Fractal web client!
+<br>
+TYPICAL ACTIONS
+<br>
+A small (one-paragraph) description of typical actions the user may want to do.
+This includes a link to the login page, which should be made extremely visible.
+<br>
+WHERE TO GET SUPPORT
+<br>
+A sentence about where to get support
+(this has to be an env variable, since it may change for each Fractal instance).
+<br>
+LINKS
+<br>
+More info on Fractal are available at: https://fractal-analytics-platform.github.io.
+Development of the different Fractal component takes place on GitHub, within the https://github.com/fractal-analytics-platform organization.
+</div>
