6ee0a36945d82348821f830843bdf580a485ceac
Sonia Zorba <sonia.zorba@mailbox.org>
2023-08-25 17:18:21 +0200

    Fixed typo

--- src/routes/projects/[projectId]/workflows/[workflowId]/+page.svelte
+++ src/routes/projects/[projectId]/workflows/[workflowId]/+page.svelte
@@ -815,7 +815,7 @@
 				<button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
 			</div>
 			<div class='modal-body'>
-				<p>Do you want to save the changes made to the arguments of the current selected workflow selectedWorkflowTask.task</p>
+				<p>Do you want to save the changes made to the arguments of the current selected workflow task?</p>
 			</div>
 			<div class='modal-footer'>
 				<button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cancel</button>
