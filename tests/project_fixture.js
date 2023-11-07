054196e8cf5d7d4a21c734bc8cdc54636d9d11af
Sonia Zorba <sonia.zorba@mailbox.org>
2023-11-07 13:19:47 +0100

    Added create project fixture

---
+++ tests/project_fixture.js
@@ -0,0 +1,44 @@
+import { test as playwrightTest, mergeTests } from '@playwright/test';
+import { test as baseTest } from './base_test.js';
+
+export class PageWithProject {
+	/**
+	 * @param {import('@playwright/test').Page} page
+	 */
+	constructor(page) {
+		this.page = page;
+		this.projectName = Math.random().toString(36).substring(7);
+	}
+
+	async createProject() {
+		await this.page.goto('/projects');
+		await this.page.locator('[name="projectName"]').fill(this.projectName);
+		const createProjectBtn = this.page.getByRole('button', { name: 'Create new project' });
+		await createProjectBtn.click();
+		await this.page.waitForURL(/\/projects\/\d+/);
+		this.url = this.page.url();
+	}
+
+	async deleteProject() {
+		await this.page.goto('/projects');
+		const rows = await this.page.getByRole('row').all();
+		for (const row of rows) {
+			if ((await row.getByRole('cell', { name: this.projectName }).count()) === 1) {
+				const deleteBtn = row.getByRole('button', { name: 'Delete' });
+				await deleteBtn.click();
+			}
+		}
+	}
+}
+
+const projectTest = playwrightTest.extend({
+	project: async ({ page }, use) => {
+		const project = new PageWithProject(page);
+		await project.createProject();
+		await use(project);
+		await project.deleteProject();
+	}
+});
+
+export const test = mergeTests(baseTest, projectTest);
+export const expect = test.expect;
