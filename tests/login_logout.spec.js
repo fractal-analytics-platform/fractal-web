2867ba609b585e5f51fb5ae8244603f6fdc1dffe
Sonia Zorba <sonia.zorba@mailbox.org>
2023-11-13 15:06:42 +0100

    Added e2e tests for session expired

--- tests/login_logout.spec.js
+++ tests/login_logout.spec.js
@@ -10,12 +10,7 @@ test('Login and Logout', async ({ page }) => {
 	await page.getByRole('link', { name: 'Login' }).first().click();
 
 	await page.waitForURL('/auth/login');
-
-	await page.getByLabel('Email address').fill('admin@fractal.xy');
-	await page.getByLabel('Password').fill('1234');
-	await page.getByRole('button', { name: 'Submit' }).click();
-
-	await page.waitForURL('/projects');
+	await login(page);
 
 	await expect(page.getByText('admin@fractal.xy')).toHaveCount(1);
 
@@ -31,3 +26,42 @@ test('Login and Logout', async ({ page }) => {
 	await expect(page.getByText('admin@fractal.xy')).toHaveCount(0);
 	await expect(page.getByText('Login')).toHaveCount(2);
 });
+
+test('Loading SSR page with expired cookied', async ({ page }) => {
+	await page.goto('/auth/login');
+	await login(page);
+	await page.context().clearCookies();
+	await page.getByRole('link', { name: 'Tasks' }).click();
+	await page.waitForSelector('text=Session expired. Please login again.');
+});
+
+test('Reloading SSR page with expired cookied', async ({ page }) => {
+	await page.goto('/auth/login');
+	await login(page);
+	await page.context().clearCookies();
+	await page.reload();
+	await page.waitForSelector('text=Session expired. Please login again.');
+});
+
+test('Loading AJAX request with expired cookied', async ({ page }) => {
+	await page.goto('/auth/login');
+	await login(page);
+	await page.context().clearCookies();
+	const projectNameInput = page.locator('[name="projectName"]');
+	await projectNameInput.fill('unauthorized');
+	await projectNameInput.blur();
+	const createProjectBtn = page.getByRole('button', { name: 'Create new project' });
+	await createProjectBtn.waitFor();
+	await createProjectBtn.click();
+	await page.waitForSelector('text=Session expired. Please login again.');
+});
+
+/**
+ * @param {import('@playwright/test').Page} page
+ */
+async function login(page) {
+	await page.getByLabel('Email address').fill('admin@fractal.xy');
+	await page.getByLabel('Password').fill('1234');
+	await page.getByRole('button', { name: 'Submit' }).click();
+	await page.waitForURL('/projects');
+}
