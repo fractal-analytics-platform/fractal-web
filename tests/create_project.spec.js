d0160e59dcb96fda862cb2f64638709acdd8ed11
Sonia Zorba <sonia.zorba@mailbox.org>
2023-11-21 15:05:29 +0100

    Updated project crud e2e test to check project info modal

--- tests/create_project.spec.js
+++ tests/create_project.spec.js
@@ -22,17 +22,45 @@ test('Create and delete a project', async ({ page }) => {
 	await page.goto('/projects');
 	await expect(page.getByRole('cell', { name: randomProjectName })).toHaveCount(1);
 
-	// Click on the delete project button related to the current project
+	// Get project row
 	const rows = await page.getByRole('row').all();
+	let projectRow;
 	for (const row of rows) {
 		if ((await row.getByRole('cell', { name: randomProjectName }).count()) === 1) {
-			const deleteBtn = row.getByRole('button', { name: 'Delete' });
-			await deleteBtn.click();
+			projectRow = row;
+			break;
 		}
 	}
+	expect(projectRow).toBeDefined();
+
+	// Open project info modal
+	const infoBtn = /** @type {import('@playwright/test').Locator} */ (projectRow).getByRole(
+		'button',
+		{ name: 'Info' }
+	);
+	await infoBtn.click();
+
+	// Wait info modal
+	let modalTitle = page.locator('.modal.show .modal-title');
+	await modalTitle.waitFor();
+	await expect(modalTitle).toHaveText('Project ' + randomProjectName);
+	const items = await page.locator('.modal.show .modal-body').getByRole('listitem').all();
+	expect(items.length).toEqual(4);
+	expect(await items[1].innerText()).toEqual(randomProjectName);
+	expect(await items[3].innerText()).toEqual('No');
+
+	let closeModalBtn = page.locator('.modal.show').getByRole('button', {name: 'Close'});
+	await closeModalBtn.click();
+
+	// Click on the delete project button related to the current project
+	const deleteBtn = /** @type {import('@playwright/test').Locator} */ (projectRow).getByRole(
+		'button',
+		{ name: 'Delete' }
+	);
+	await deleteBtn.click();
 
 	// Wait confirm action modal
-	const modalTitle = page.locator('.modal.show .modal-title');
+	modalTitle = page.locator('.modal.show .modal-title');
 	await modalTitle.waitFor();
 	await expect(modalTitle).toHaveText('Confirm action');
 	await expect(page.locator('.modal.show .modal-body')).toContainText(
