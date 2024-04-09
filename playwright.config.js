5566483fb0e89bd555e1443e6d7f47de13d895be
Sonia Zorba <sonia.zorba@mailbox.org>
2024-04-09 11:27:54 +0200

    Used fractal-server 2.0.0a3 in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
 
 	webServer: [
 		{
-			command: './tests/start-test-server.sh 2.0.0a2',
+			command: './tests/start-test-server.sh 2.0.0a3',
 			port: 8000,
 			waitForPort: true,
 			stdout: 'pipe',
