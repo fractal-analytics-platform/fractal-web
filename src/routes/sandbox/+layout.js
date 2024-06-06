ec16b3a3ac8ffcf103463e92594e8ad05f1cd9f0
Sonia Zorba <sonia.zorba@mailbox.org>
2024-06-06 11:25:07 +0200

    Added docs about JSON Schema Sandbox page

--- src/routes/sandbox/+layout.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export async function load() {
-	if (import.meta.env.MODE !== 'development') {
-		console.warn('Sandbox page is disabled in development mode');
-		throw redirect(307, '/');
-	}
-}
