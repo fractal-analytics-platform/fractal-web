febe147c38906bfd6d540d74f7342feac808726e
Sonia Zorba <sonia.zorba@mailbox.org>
2024-09-27 14:09:54 +0200

    Sorted groups by name in admin groups page

--- src/routes/v2/admin/groups/+page.server.js
+++ src/routes/v2/admin/groups/+page.server.js
@@ -1,3 +1,4 @@
+import { sortGroupByNameComparator } from '$lib/common/user_utilities';
 import { listGroups } from '$lib/server/api/auth_api';
 import { getLogger } from '$lib/server/logger.js';
 
@@ -7,6 +8,8 @@ export async function load({ fetch }) {
 	logger.trace('Load groups page');
 
 	const groups = await listGroups(fetch, true);
+
+	groups.sort(sortGroupByNameComparator);
 
 	return {
 		groups
