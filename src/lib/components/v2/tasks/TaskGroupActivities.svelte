552e0a4e3d0c423eb177ff53fa9a7063070266e2
Sonia Zorba <sonia.zorba@mailbox.org>
2024-12-03 10:59:28 +0100

    Added user column and sorted users by email in v2/admin/task-groups/activities

--- src/lib/components/v2/tasks/TaskGroupActivities.svelte
+++ src/lib/components/v2/tasks/TaskGroupActivities.svelte
@@ -9,11 +9,17 @@
 	import TaskGroupActivityLogsModal from './TaskGroupActivityLogsModal.svelte';
 	import { getTimestamp } from '$lib/common/component_utilities';
 	import { page } from '$app/stores';
+	import { sortUserByEmailComparator } from '$lib/common/user_utilities';
 
 	export let admin = false;
 
-	/** @type {import('$lib/types').User[]} */
+	/** @type {Array<import('$lib/types').User & {id: number}>} */
 	export let users = [];
+	/** @type {Array<import('$lib/types').User & {id: number}>} */
+	let sortedUsers = [];
+	$: if (users) {
+		sortedUsers = [...users].sort(sortUserByEmailComparator);
+	}
 
 	/** @type {import('$lib/types-v2').TaskGroupActivityV2[]} */
 	let results = [];
@@ -152,6 +158,13 @@
 		);
 	}
 
+	/**
+	 * @param {number} userId
+	 */
+	function getUserEmail(userId) {
+		return users.find((u) => u.id === userId)?.email || '-';
+	}
+
 	onMount(async () => {
 		const activityId = $page.url.searchParams.get('activity_id');
 		if (activityId && !isNaN(Number(activityId))) {
@@ -225,7 +238,7 @@
 				<label class="input-group-text" for="user"> User </label>
 				<select class="form-select" id="user" bind:value={user_id}>
 					<option value={null}>Select...</option>
-					{#each users as user}
+					{#each sortedUsers as user}
 						<option value={user.id}>{user.email}</option>
 					{/each}
 				</select>
@@ -262,6 +275,9 @@
 				<th>Status</th>
 				<th>Started</th>
 				<th>Ended</th>
+				{#if admin}
+					<th>User</th>
+				{/if}
 				<th>Options</th>
 			</tr>
 		</thead>
@@ -286,6 +302,9 @@
 							? new Date(taskGroupActivity.timestamp_ended).toLocaleString()
 							: '-'}
 					</td>
+					{#if admin}
+						<td>{getUserEmail(taskGroupActivity.user_id)}</td>
+					{/if}
 					<td>
 						{#if taskGroupActivity.status !== 'pending' && taskGroupActivity.log}
 							<button
