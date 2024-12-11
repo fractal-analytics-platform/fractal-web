6995a886528ff88116b2c5f6285c75db58d0882f
Sonia Zorba <sonia.zorba@mailbox.org>
2024-12-11 16:03:46 +0100

    Fixed task group activity wrong admin API call

--- src/lib/components/v2/tasks/TaskGroupActivities.svelte
+++ src/lib/components/v2/tasks/TaskGroupActivities.svelte
@@ -321,4 +321,4 @@
 	</table>
 </div>
 
-<TaskGroupActivityLogsModal bind:this={taskGroupActivityLogsModal} admin={true} />
+<TaskGroupActivityLogsModal bind:this={taskGroupActivityLogsModal} {admin} />
