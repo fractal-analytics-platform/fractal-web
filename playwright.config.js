cc91411421f22c7b5f39feb65fd5cf0a69374dcc
Sonia Zorba <sonia.zorba@mailbox.org>
2025-01-28 13:53:42 +0100

    Updated playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
 
 	webServer: [
 		{
-			command: './tests/start-test-server.sh --branch dev-2.11',
+			command: './tests/start-test-server.sh --branch main',
 			port: 8000,
 			waitForPort: true,
 			stdout: 'pipe',
