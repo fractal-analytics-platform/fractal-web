3e8360c5487fb832478a577df9d6792ae30d4c65
Sonia Zorba <sonia.zorba@mailbox.org>
2025-04-10 15:41:58 +0200

    Added clipboard permission to get token test

--- tests/get_token_test.spec.js
+++ tests/get_token_test.spec.js
@@ -1,6 +1,10 @@
 import { expect, test } from '@playwright/test';
 import { waitPageLoading } from './utils.js';
 
+test.use({
+	permissions: ['clipboard-write']
+});
+
 test('Copy token test', async ({ page }) => {
 	await page.goto('/profile');
 	await waitPageLoading(page);
