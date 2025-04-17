aeb142881598bbba4ced319a9f2fc11b3ed54524
Sonia Zorba <sonia.zorba@mailbox.org>
2025-04-17 12:35:47 +0200

    Added more e2e tests

---
+++ tests/v2/run_workflow_specify_type_filters.spec.js
@@ -0,0 +1,81 @@
+import { selectSlimSelect, waitModalClosed, waitPageLoading } from '../utils.js';
+import { createDataset } from './dataset_utils.js';
+import { expect, test } from './workflow_fixture.js';
+import { waitTaskSubmitted, waitTasksSuccess } from './workflow_task_utils.js';
+
+test('Warning message "You are trying to run a workflow without specifying what type of images should be processed"', async ({
+	page,
+	workflow
+}) => {
+	await page.waitForURL(workflow.url);
+	await waitPageLoading(page);
+
+	await page.goto(`/v2/projects/${workflow.projectId}`);
+	await waitPageLoading(page);
+
+	const modal = page.locator('.modal.show');
+
+	let zarrDir;
+
+	await test.step('Create test dataset', async () => {
+		const dataset = await createDataset(page, workflow.projectId);
+		zarrDir = dataset.zarrDir;
+	});
+
+	await test.step('Prepare workflow', async () => {
+		await page.goto(workflow.url);
+		await waitPageLoading(page);
+		await workflow.addTask('create_ome_zarr_compound');
+		await workflow.addTask('cellpose_segmentation');
+		await workflow.addTask('MIP_compound');
+		await workflow.selectTask('create_ome_zarr_compound');
+		await page.getByRole('textbox', { name: 'Image Dir' }).fill(zarrDir);
+		await page.getByRole('button', { name: 'Save changes' }).click();
+		await page.getByText('Arguments changes saved successfully').waitFor();
+	});
+
+	await test.step('Run workflow', async () => {
+		await page.getByRole('button', { name: 'Run workflow' }).click();
+		await modal.waitFor();
+		await modal.getByRole('button', { name: 'Run', exact: true }).click();
+		await modal.getByRole('button', { name: 'Confirm' }).click();
+		await waitModalClosed(page);
+		await waitTaskSubmitted(page);
+		await waitTasksSuccess(page);
+	});
+
+	await test.step('Trigger type filters warning message', async () => {
+		await page.getByRole('button', { name: 'Continue workflow' }).click();
+		await modal.waitFor();
+		await modal
+			.getByRole('combobox', { name: 'Start workflow at' })
+			.selectOption('cellpose_segmentation');
+		await expect(modal.getByText('Total results: 4')).toBeVisible();
+		await modal.getByRole('button', { name: 'Run', exact: true }).click();
+		await expect(
+			modal.getByText(/You are trying to run a workflow without specifying what type/)
+		).toBeVisible();
+	});
+
+	await test.step('Continue anyway and back', async () => {
+		await modal.getByRole('button', { name: 'Continue anyway' }).click();
+		await expect(modal.getByText('This job will process 4 images.')).toBeVisible();
+		await modal.getByRole('button', { name: 'Cancel' }).click();
+		await expect(modal.getByText('Total results: 4')).toBeVisible();
+		await modal.getByRole('button', { name: 'Run', exact: true }).click();
+		await expect(
+			modal.getByText(/You are trying to run a workflow without specifying what type/)
+		).toBeVisible();
+	});
+
+	await test.step('Select type', async () => {
+		await selectSlimSelect(page, modal.getByLabel('Selector for type 3D'), 'True');
+		await modal.getByRole('button', { name: 'Apply' }).click();
+		await expect(modal.getByText('Total results: 2')).toBeVisible();
+		await modal.getByRole('button', { name: 'Run', exact: true }).click();
+		await expect(modal.getByText(/the following filters are applied/)).toBeVisible();
+		await expect(
+			modal.locator('li').filter({ hasText: '3D:' }).locator('[aria-checked="true"]')
+		).toBeVisible();
+	});
+});
---
+++ tests/v2/workflow_task_failed_images.spec.js
@@ -0,0 +1,68 @@
+import { expect, test } from './workflow_fixture.js';
+import { waitModalClosed, waitPageLoading } from '../utils.js';
+import { createDataset } from './dataset_utils.js';
+import { waitTaskFailure, waitTasksSuccess } from './workflow_task_utils.js';
+
+test('Workflow task with failed images', async ({ page, workflow }) => {
+	await page.waitForURL(workflow.url);
+	await waitPageLoading(page);
+
+	test.slow();
+	const modal = page.locator('.modal.show');
+
+	/** @type {string} */
+	let zarrDir;
+	await test.step('Create test dataset', async () => {
+		const dataset = await createDataset(page, workflow.projectId);
+		zarrDir = dataset.zarrDir;
+	});
+
+	await test.step('Open workflow page and configure tasks', async () => {
+		await page.goto(workflow.url);
+		await waitPageLoading(page);
+		await workflow.addTask('create_ome_zarr_compound');
+		await workflow.addTask('MIP_compound');
+		await workflow.selectTask('create_ome_zarr_compound');
+		await page.getByRole('textbox', { name: 'Image Dir' }).fill(zarrDir);
+		await page.getByRole('button', { name: 'Save changes' }).click();
+		await expect(page.getByRole('button', { name: 'Save changes' })).toBeDisabled();
+	});
+
+	await test.step('Run workflow', async () => {
+		await page.getByRole('button', { name: 'Run workflow' }).click();
+		await modal.waitFor();
+		await modal.getByRole('button', { name: 'Run' }).click();
+		await modal.getByRole('button', { name: 'Confirm' }).click();
+		await waitModalClosed(page);
+		await waitTasksSuccess(page);
+	});
+
+	await test.step('Continue workflow causing failure', async () => {
+		await page.getByRole('button', { name: 'Continue workflow' }).click();
+		await modal.waitFor();
+		await modal.getByRole('combobox', { name: 'Start workflow at' }).selectOption('MIP_compound');
+		await expect(modal.getByText('Total results: 2')).toBeVisible();
+		await expect(modal.getByRole('button', { name: 'Apply' })).not.toBeEnabled();
+		await modal.getByRole('button', { name: 'Run' }).click();
+		await modal.getByRole('button', { name: 'Confirm' }).click();
+		await waitModalClosed(page);
+		await waitTaskFailure(page);
+	});
+
+	await test.step('Open failed logs modal', async () => {
+		await page.locator('[aria-label="Show runs"]').nth(1).click();
+		await page.locator('[aria-label="Failed images"]').first().click();
+		await modal.waitFor();
+		await expect(modal.getByText('Images')).toBeVisible();
+		await expect(modal.getByText('Total results: 2')).toBeVisible();
+		await modal
+			.getByRole('row', { name: 'failed' })
+			.first()
+			.getByRole('button', { name: 'Logs' })
+			.click();
+		await expect(modal.getByText(/click here to expand/)).toBeVisible();
+		await expect(modal.getByText('FileExistsError')).toBeVisible();
+		await modal.getByRole('button', { name: 'Close' }).click();
+		await waitModalClosed(page);
+	});
+});
