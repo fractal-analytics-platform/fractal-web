d6b7ccac3b2a1eb6b3303133755d5ece6de3e3da
Sonia Zorba <sonia.zorba@mailbox.org>
2025-10-02 15:15:47 +0200

    Removed unused code

--- src/lib/server/api/v2/admin_api.js
+++ src/lib/server/api/v2/admin_api.js
@@ -5,26 +5,6 @@ import { getLogger } from '$lib/server/logger.js';
 const logger = getLogger('admin API [v2]');
 
 /**
- * Fetches the list of projects from the server
- * @param {typeof fetch} fetch
- * @returns {Promise<import('fractal-components/types/api').ProjectV2[]>}
- */
-export async function listProjects(fetch) {
-	logger.debug('Fetching the list of projects');
-	const response = await fetch(`${env.FRACTAL_SERVER_HOST}/admin/v2/project/`, {
-		method: 'GET',
-		credentials: 'include'
-	});
-
-	if (!response.ok) {
-		logger.error('Unable to fetch the list of projects');
-		await responseError(response);
-	}
-
-	return await response.json();
-}
-
-/**
  * Request to impersonate user
  * @param {typeof fetch} fetch
  * @param {number} userId
--- src/routes/v2/admin/jobs/+page.server.js
+++ src/routes/v2/admin/jobs/+page.server.js
@@ -1,5 +1,4 @@
 import { listUsers } from '$lib/server/api/auth_api.js';
-import { listProjects } from '$lib/server/api/v2/admin_api.js';
 import { getLogger } from '$lib/server/logger.js';
 
 const logger = getLogger('admin jobs page [v2]');
@@ -7,11 +6,9 @@ const logger = getLogger('admin jobs page [v2]');
 export async function load({ fetch }) {
 	logger.trace('Loading admin jobs page');
 
-	const projects = await listProjects(fetch);
 	const users = await listUsers(fetch);
 
 	return {
-		projects,
 		users
 	};
 }
