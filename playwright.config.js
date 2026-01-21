7d0b183ee289afa8d627161808a0f80e796f40eb
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-01-21 12:39:16 +0100

    run tests on branch 3143-drop-support-for-task-source

--- playwright.config.js
+++ playwright.config.js
@@ -96,7 +96,7 @@ export default defineConfig({
 
 	webServer: [
 		{
-			command: './tests/start-test-server.sh --branch main',
+			command: './tests/start-test-server.sh --branch 3143-drop-support-for-task-source',
 			port: 8000,
 			waitForPort: true,
 			stdout: 'pipe',
