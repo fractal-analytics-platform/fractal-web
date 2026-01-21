e6f4a4551280dfad592c3786597d4537fa22f1bd
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-01-21 12:37:07 +0100

    use branch main

--- playwright.config.js
+++ playwright.config.js
@@ -96,7 +96,7 @@ export default defineConfig({
 
 	webServer: [
 		{
-			command: './tests/start-test-server.sh --branch 3126-support-guest-api-user',
+			command: './tests/start-test-server.sh --branch main',
 			port: 8000,
 			waitForPort: true,
 			stdout: 'pipe',
