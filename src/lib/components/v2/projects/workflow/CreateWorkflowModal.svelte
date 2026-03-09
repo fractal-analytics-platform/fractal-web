f19aade17ff733d76923cd3423894b3465b0a122
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-03-09 10:39:33 +0100

    extract import flexibily as standalone component

--- src/lib/components/v2/projects/workflow/CreateWorkflowModal.svelte
+++ src/lib/components/v2/projects/workflow/CreateWorkflowModal.svelte
@@ -7,6 +7,7 @@
 	import { normalizePayload } from 'fractal-components';
 	import BooleanIcon from 'fractal-components/common/BooleanIcon.svelte';
 	import TemplatesTable from '../../templates/TemplatesTable.svelte';
+	import WorkflowImportFlexibility from './WorkflowImportFlexibility.svelte';
 
 	/**
 	 * @typedef {Object} Props
@@ -406,110 +407,13 @@
 					Import workflow
 				</button>
 			{:else}
-					<hr />
-					<p>
-						Some of the requested tasks are not available on this Fractal instance.
-						You can collect missing task packages at the <a href="/v2/tasks/management">Tasks management</a> page, 
-						or select one of the available versions listed below.
-					</p>
-	
-					<div class="mb-3">
-						<div class="form-check">
-							<input
-								class="form-check-input"
-								type="checkbox"
-								id="checkIncludeOlderVersions"
-								bind:checked={includeOlderVersions}
-							/>
-							<label class="form-check-label" for="checkIncludeOlderVersions">
-								<i>Include older versions</i>
-							</label>
-						</div>
-					</div>
-	
-				{#each workflowImportErrorData as data, index (index)}
-					<hr />	
-					<section class="task-to-import">
-						<div
-							style="display: flex; align-items: center; gap: 8px;"
-						>
-							<div>
-								{#if data.outcome === "success" || selectedVersions[index]}
-									<BooleanIcon value={true} />
-								{:else}
-									<BooleanIcon value={false} />
-								{/if}
-							</div>
-							<div>
-								Task <strong>{data.task_name}</strong> <span>({data.pkg_name})</span>
-							</div>
-						</div>
-						<div>Requested version: {data.version || '-'}</div>
-						<div>
-						{#if data.outcome !== "success"}
-							{#if data.available_tasks.some(task => !data.version || includeOlderVersions || (!includeOlderVersions && task.version > data.version))}
-							<div class="row row-cols-lg-auto g-3 align-items-center">
-							  <div class="col-12">
-								Available versions:
-							</div>
-							<div class="col-12">
-								<select
-									bind:value={selectedVersions[index]}
-									style="width: 15ch"
-									class="form-select"
-								>
-									<option value={undefined}>Select...</option>
-									{#each [...data.available_tasks].sort(
-										(a, b) => a.version.localeCompare(b.version)
-									) as task, i (i)}
-										{#if !data.version || includeOlderVersions || (!includeOlderVersions && task.version > data.version)}
-											<option
-												value={task.version}
-												title={task.active ? "" : "Not active"}
-											>
-													{task.version}{task.active ? "" : " ⚠️"}
-											</option>
-										{/if}
-									{/each}
-								</select>
-							</div>
-							</div>
-							{:else}
-								No available versions.<br>
-								You should collect the task package at the <a href="/v2/tasks/management">Tasks management</a>
-								page{#if !includeOlderVersions}, or try including older versions{/if}.
-							{/if}
-						{/if}
-						</div>
-					</section>
-					{/each}
-				<hr />
-				<div>
-					<button
-						class="btn btn-primary mt-2"
-						disabled={creating || selectedVersions.some(v => v === undefined)}
-					>
-						{#if creating}
-							<span
-								class="spinner-border spinner-border-sm"
-								role="status"
-								aria-hidden="true"
-							></span>
-						{/if}
-						Import workflow
-					</button>
-					<button
-						class="btn btn-danger mt-2"
-						onclick={() => {
-							workflowImportErrorData = undefined;
-							workflowMetadata = undefined;
-							includeOlderVersions = false;
-						}}
-					>
-						Cancel
-					</button>
-				</div>
-				
+				<WorkflowImportFlexibility
+					bind:workflowImportErrorData
+					bind:selectedVersions
+					bind:includeOlderVersions
+					bind:workflowMetadata
+					{creating}
+				/>
 			{/if}
 			<div class="mt-2" id="errorAlert-createWorkflowModal"></div>
 		</form>
