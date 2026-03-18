2b192cac5403b2ae4f031b919d5f5c9ca61b78ab
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-03-18 12:22:05 +0100

    remove commented block

--- templates-list/src/lib/TemplatesList.svelte
+++ templates-list/src/lib/TemplatesList.svelte
@@ -250,10 +250,6 @@
 <div class="modal modal-xl" id="template-info-modal" tabindex="-1">
 	<div class="modal-dialog modal-dialog-scrollable">
 		<div class="modal-content">
-			<!-- <div class="modal-header">
-				<h5 class="modal-title">Template info</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-			</div> -->
 			<div class="modal-body">
 				<ul class="list-group">
 					<li class="list-group-item text-bg-light">
