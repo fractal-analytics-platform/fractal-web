2b46022a59ef7a2449e2d7eec2a09acc3803d215
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-03-19 10:59:44 +0100

    fix eslint

--- src/routes/v2/templates/+page.server.js
+++ src/routes/v2/templates/+page.server.js
@@ -1,7 +1,5 @@
-import { getSortGroupByNameAllFirstComparator } from '$lib/components/admin/user_utilities';
 import { getCurrentUser } from '$lib/server/api/auth_api';
 import { getTemplates } from '$lib/server/api/v2/template_api';
-import { env } from '$env/dynamic/private';
 
 export async function load({ fetch }) {
 	const templatePage = await getTemplates(fetch);
