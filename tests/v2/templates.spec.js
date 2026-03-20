daad3d1cb63ebce5368a6f32599f78a4d47ba876
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-03-20 10:41:20 +0100

    test template info and template update

--- tests/v2/templates.spec.js
+++ tests/v2/templates.spec.js
@@ -61,8 +61,61 @@ test('Use template page', async ({ page }) => {
         await modal2.getByLabel('User Group').selectOption({ label: 'All' });
         await modal2.getByRole('button', { name: 'Create template' }).click();
         await waitPageLoading(page);
+    });
+    
+    await expect(page).toHaveURL(/\/v2\/templates\?template_id=/);
+    const url = new URL(page.url());
+    const templateId = url.searchParams.get('template_id');
 
-        //  Now we are at /v2/templates?template_id=1
+    await test.step('Check template info modal', async () => {
+
+        await page.getByRole('button', { name: 'Info' }).click();
+        const modal = await waitModal(page);
+
+        await expect(page.locator('.list-group-item.text-bg-light')).toContainText(
+            [
+                'Template ID',
+                'User email',
+                'Name',
+                'Version',
+                'User group',
+                'Description',
+                'Creation timestamp',
+                'Last use timestamp'
+            ]
+        );
+        const values = page.locator('.list-group-item:not(.text-bg-light)');
+        await expect(values.nth(0).locator('span')).toHaveText(String(templateId));
+        await expect(values.nth(1).locator('span')).toHaveText('admin@fractal.xy');
+        await expect(values.nth(2).locator('span')).toHaveText(workflow.name);
+        await expect(values.nth(3).locator('span')).toHaveText("1");
+        await expect(values.nth(4).locator('span')).toHaveText("All");
+        await expect(values.nth(5).locator('span')).toHaveText("This is the template description.");
+        await expect(values.nth(6).locator('span')).toHaveText(
+            await values.nth(7).locator('span').allInnerTexts()
+        );
+        await modal.getByRole('button', { name: 'Close' }).click();
+        await waitModalClosed(page);
+    });
+
+    await test.step('Edit template', async () => {
+
+        await page.getByRole('button', { name: 'Edit' }).click();
+        const modal = await waitModal(page);
+
+        await modal.getByLabel('User Group').selectOption({ label: undefined });
+        await page.getByRole('textbox', { name: 'Description' }).fill('New description.');
+        await modal.getByRole('button', { name: 'Save' }).click();
+        await waitModalClosed(page);
+
+        await page.getByRole('button', { name: 'Info' }).click();
+        await waitModal(page);
+        const values = page.locator('.list-group-item:not(.text-bg-light)');
+        await expect(values.nth(4).locator('span')).toHaveText("-");
+        await expect(values.nth(5).locator('span')).toHaveText("New description.");
+
+        await modal.getByRole('button', { name: 'Close' }).click();
+        await waitModalClosed(page);
     });
 
 });
