ef2f20789bc36eef83891cde5526b9d7516317bb
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-03-30 15:40:35 +0200

    revert playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -96,7 +96,7 @@ export default defineConfig({
 
 	webServer: [
 		{
-			command: './tests/start-test-server.sh --branch 3212-restrict-which-individual-task-properties-can-be-edited',
+			command: './tests/start-test-server.sh --branch main',
 			port: 8000,
 			waitForPort: true,
 			stdout: 'pipe',
