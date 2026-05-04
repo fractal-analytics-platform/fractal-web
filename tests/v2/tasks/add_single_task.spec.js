88991da373cb603ee1e450e9a0963e0ac3876d45
Sonia Zorba <sonia.zorba@mailbox.org>
2026-05-04 11:00:34 +0200

    Used mode: 'parallel' for add_single_task.spec.js

--- tests/v2/tasks/add_single_task.spec.js
+++ tests/v2/tasks/add_single_task.spec.js
@@ -1,5 +1,6 @@
 import { expect, test } from '@playwright/test';
 import {
+	getRandomName,
 	setUploadFile,
 	waitModalClosed,
 	waitPageLoading,
@@ -12,29 +13,21 @@ import { collapseExpandedRows, deleteTask } from '../../utils/v2/task.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const validMetaFile = path.join(__dirname, '..', 'data', 'meta.json');
-const validArgsSchemaFile = path.join(__dirname, '..', 'data', 'create-ome-zarr-schema.json');
-const brokenJson = path.join(__dirname, '..', 'data', 'broken.json');
-const invalidArgsSchema = path.join(__dirname, '..', 'data', 'invalid-schema.json');
-const pydanticV1Schema = path.join(__dirname, '..', 'data', 'pydantic-v1-schema.json');
-const pydanticV2Schema = path.join(__dirname, '..', 'data', 'pydantic-v2-schema.json');
+const validMetaFile = path.join(__dirname, '..', '..', 'data', 'meta.json');
+const validArgsSchemaFile = path.join(__dirname, '..', '..', 'data', 'create-ome-zarr-schema.json');
+const brokenJson = path.join(__dirname, '..', '..', 'data', 'broken.json');
+const invalidArgsSchema = path.join(__dirname, '..', '..', 'data', 'invalid-schema.json');
+const pydanticV1Schema = path.join(__dirname, '..', '..', 'data', 'pydantic-v1-schema.json');
+const pydanticV2Schema = path.join(__dirname, '..', '..', 'data', 'pydantic-v2-schema.json');
 
-test('Add single tasks [v2]', async ({ page }) => {
-	await page.goto('/v2/tasks/management');
-	await waitPageLoading(page);
+test.describe('Add single tasks [v2]', () => {
+	test.describe.configure({ mode: 'parallel' });
 
-	test.slow();
+	test('Create non parallel task', async ({ page }) => {
+		await openSingleTaskForm(page);
 
-	await test.step('Select "Add a single task" form', async () => {
-		await page.getByText('Single task').click();
-	});
-
-	const createBtn = page.getByRole('button', { name: /^Create$/ });
-
-	const randomTaskName1 = Math.random().toString(36).substring(7);
-
-	await test.step('Create non parallel task', async () => {
-		await page.getByRole('textbox', { name: 'Task name' }).fill(randomTaskName1);
+		const taskName = getRandomName();
+		await page.getByRole('textbox', { name: 'Task name' }).fill(taskName);
 		await page.getByRole('textbox', { name: 'Command non parallel' }).fill('/tmp/test');
 		await page.getByRole('button', { name: 'Add input type' }).click();
 		await page.getByPlaceholder('Key').fill('input_type');
@@ -47,56 +40,86 @@ test('Add single tasks [v2]', async ({ page }) => {
 		await page.getByText('Documentation').click();
 		await page.getByRole('textbox', { name: 'Docs info' }).fill('# Task title');
 		await page.getByRole('textbox', { name: 'Docs link' }).fill('https://www.example.com');
-		await createBtn.click();
+		await page.getByRole('button', { name: /^Create$/ }).click();
 		await page.getByText('Task created successfully').waitFor();
 
-		const task = await getCreatedTaskModalData(page, randomTaskName1, 'non_parallel');
-		expect(task.name).toEqual(randomTaskName1);
+		const task = await getCreatedTaskModalData(page, taskName, 'non_parallel');
+		expect(task.name).toEqual(taskName);
 		expect(task.command_non_parallel).toEqual('/tmp/test');
 		expect(task.input_types).toContain('input_type');
 		expect(task.output_types).toContain('output_type');
 		expect(task.args_schema_version).toEqual('pydantic_v2');
 		expect(task.docs_info).toEqual('Task title');
 		expect(task.docs_link).toEqual('https://www.example.com');
+
+		await deleteTask(page, taskName);
 	});
 
-	const randomTaskName2 = Math.random().toString(36).substring(7);
+	test('Create parallel task', async ({ page }) => {
+		await openSingleTaskForm(page);
+		const taskName = getRandomName();
 
-	await test.step('Create parallel task', async () => {
-		await page.getByRole('combobox', { name: 'Task type' }).selectOption('Parallel');
+		await test.step('Create parallel task v0.0.1', async () => {
+			await page.getByRole('combobox', { name: 'Task type' }).selectOption('Parallel');
 
-		await page.getByRole('textbox', { name: 'Task name' }).fill(randomTaskName2);
-		await page.getByRole('textbox', { name: 'Command parallel' }).fill('/tmp/test');
-		await page.getByRole('button', { name: 'Add input type' }).click();
-		await page.getByPlaceholder('Key').fill('input_type');
-		await page.getByLabel('Value for input_type').check();
-		await page.getByRole('button', { name: 'Add output type' }).click();
-		await page.getByPlaceholder('Key').nth(1).fill('output_type');
-		await page.getByRole('textbox', { name: 'Version' }).fill('0.0.1');
-		await setUploadFile(page, 'Upload parallel meta file', validMetaFile);
-		await setUploadFile(page, 'Upload parallel args schema', validArgsSchemaFile);
-		await page.getByText('Documentation').click();
-		await page.getByRole('textbox', { name: 'Docs info' }).fill('# Task title');
-		await page.getByRole('textbox', { name: 'Docs link' }).fill('https://www.example.com');
-		await createBtn.click();
-		await page.getByText('Task created successfully').waitFor();
+			await page.getByRole('textbox', { name: 'Task name' }).fill(taskName);
+			await page.getByRole('textbox', { name: 'Command parallel' }).fill('/tmp/test');
+			await page.getByRole('button', { name: 'Add input type' }).click();
+			await page.getByPlaceholder('Key').fill('input_type');
+			await page.getByLabel('Value for input_type').check();
+			await page.getByRole('button', { name: 'Add output type' }).click();
+			await page.getByPlaceholder('Key').nth(1).fill('output_type');
+			await page.getByRole('textbox', { name: 'Version' }).fill('0.0.1');
+			await setUploadFile(page, 'Upload parallel meta file', validMetaFile);
+			await setUploadFile(page, 'Upload parallel args schema', validArgsSchemaFile);
+			await page.getByText('Documentation').click();
+			await page.getByRole('textbox', { name: 'Docs info' }).fill('# Task title');
+			await page.getByRole('textbox', { name: 'Docs link' }).fill('https://www.example.com');
+			await page.getByRole('button', { name: /^Create$/ }).click();
+			await page.getByText('Task created successfully').waitFor();
 
-		const task = await getCreatedTaskModalData(page, randomTaskName2, 'parallel');
-		expect(task.name).toEqual(randomTaskName2);
-		expect(task.command_parallel).toEqual('/tmp/test');
-		expect(task.input_types).toContain('input_type');
-		expect(task.output_types).toContain('output_type');
-		expect(task.args_schema_version).toEqual('pydantic_v2');
-		expect(task.docs_info).toEqual('Task title');
-		expect(task.docs_link).toEqual('https://www.example.com');
+			const task = await getCreatedTaskModalData(page, taskName, 'parallel');
+			expect(task.name).toEqual(taskName);
+			expect(task.command_parallel).toEqual('/tmp/test');
+			expect(task.input_types).toContain('input_type');
+			expect(task.output_types).toContain('output_type');
+			expect(task.args_schema_version).toEqual('pydantic_v2');
+			expect(task.docs_info).toEqual('Task title');
+			expect(task.docs_link).toEqual('https://www.example.com');
+		});
+
+		await test.step('Create parallel task v0.0.2', async () => {
+			await page.getByRole('combobox', { name: 'Task type' }).selectOption('Parallel');
+			await page.getByRole('textbox', { name: 'Task name' }).fill(taskName);
+			await page.getByRole('textbox', { name: 'Command parallel' }).fill('/tmp/test2');
+			await page.getByRole('textbox', { name: 'Version' }).fill('0.0.2');
+			await page.getByRole('button', { name: /^Create$/ }).click();
+			await page.getByText('Task created successfully').waitFor();
+			const versionSelector = page.getByRole('row', { name: taskName }).getByRole('combobox');
+			await expect(versionSelector).toHaveValue('0.0.2');
+			let task = await getCreatedTaskModalData(page, taskName, 'parallel');
+			expect(task.name).toEqual(taskName);
+			expect(task.command_parallel).toEqual('/tmp/test2');
+			expect(task.args_schema_version).toEqual('-');
+			await versionSelector.selectOption('0.0.1');
+			await expect(versionSelector).toHaveValue('0.0.1');
+			task = await getCreatedTaskModalData(page, taskName, 'parallel');
+			expect(task.name).toEqual(taskName);
+			expect(task.command_parallel).toEqual('/tmp/test');
+			expect(task.args_schema_version).toEqual('pydantic_v2');
+		});
+
+		await deleteTask(page, taskName, '0.0.2');
+		await deleteTask(page, taskName, '0.0.1');
 	});
 
-	const randomTaskName3 = Math.random().toString(36).substring(7);
+	test('Create compound task', async ({ page }) => {
+		await openSingleTaskForm(page);
+		const taskName = getRandomName();
 
-	await test.step('Create compound task', async () => {
 		await page.getByRole('combobox', { name: 'Task type' }).selectOption('Compound');
 
-		await page.getByRole('textbox', { name: 'Task name' }).fill(randomTaskName3);
+		await page.getByRole('textbox', { name: 'Task name' }).fill(taskName);
 		await page.getByRole('textbox', { name: 'Command non parallel' }).fill('/tmp/test-np');
 		await page.getByRole('textbox', { name: 'Command parallel' }).fill('/tmp/test-p');
 		await page.getByRole('button', { name: 'Add input type' }).click();
@@ -112,11 +135,11 @@ test('Add single tasks [v2]', async ({ page }) => {
 		await page.getByText('Documentation').click();
 		await page.getByRole('textbox', { name: 'Docs info' }).fill('# Task title');
 		await page.getByRole('textbox', { name: 'Docs link' }).fill('https://www.example.com');
-		await createBtn.click();
+		await page.getByRole('button', { name: /^Create$/ }).click();
 		await page.getByText('Task created successfully').waitFor();
 
-		const task = await getCreatedTaskModalData(page, randomTaskName3, 'compound');
-		expect(task.name).toEqual(randomTaskName3);
+		const task = await getCreatedTaskModalData(page, taskName, 'compound');
+		expect(task.name).toEqual(taskName);
 		expect(task.command_non_parallel).toEqual('/tmp/test-np');
 		expect(task.command_parallel).toEqual('/tmp/test-p');
 		expect(task.input_types).toContain('input_type');
@@ -124,122 +147,90 @@ test('Add single tasks [v2]', async ({ page }) => {
 		expect(task.args_schema_version).toEqual('pydantic_v2');
 		expect(task.docs_info).toEqual('Task title');
 		expect(task.docs_link).toEqual('https://www.example.com');
+
+		await deleteTask(page, taskName);
 	});
 
-	const randomTaskName4 = Math.random().toString(36).substring(7);
+	test('Create task validation', async ({ page }) => {
+		await openSingleTaskForm(page);
+		const taskName = getRandomName();
 
-	const addInputTypeBtn = page.getByRole('button', { name: 'Add input type' });
-	const addOutputTypeBtn = page.getByRole('button', { name: 'Add output type' });
+		const addInputTypeBtn = page.getByRole('button', { name: 'Add input type' });
+		const addOutputTypeBtn = page.getByRole('button', { name: 'Add output type' });
+		const createBtn = page.getByRole('button', { name: /^Create$/ });
 
-	await test.step('Attempt to create task with types with empty keys', async () => {
-		await page.getByRole('textbox', { name: 'Task name' }).fill(randomTaskName4);
-		await page.getByRole('textbox', { name: 'Command non parallel' }).fill('/tmp/test-np');
-		await page.getByRole('textbox', { name: 'Command parallel' }).fill('/tmp/test-p');
-		await addInputTypeBtn.click();
-		await addOutputTypeBtn.click();
-		await createBtn.click();
-		await page.getByText('Key is required').first().waitFor();
-		expect(await page.getByText('Key is required').count()).toEqual(2);
-	});
+		await test.step('Attempt to create task with types with empty keys', async () => {
+			await page.getByRole('combobox', { name: 'Task type' }).selectOption('Compound');
+			await page.getByRole('textbox', { name: 'Task name' }).fill(taskName);
+			await page.getByRole('textbox', { name: 'Command non parallel' }).fill('/tmp/test-np');
+			await page.getByRole('textbox', { name: 'Command parallel' }).fill('/tmp/test-p');
+			await addInputTypeBtn.click();
+			await addOutputTypeBtn.click();
+			await createBtn.click();
+			await page.getByText('Key is required').first().waitFor();
+			expect(await page.getByText('Key is required').count()).toEqual(2);
+		});
 
-	await test.step('Attempt to create task with types with duplicated keys', async () => {
-		await addInputTypeBtn.click();
-		await addOutputTypeBtn.click();
-		const inputs = await page.getByPlaceholder('Key').all();
-		for (const input of inputs) {
-			await input.fill('SameKey');
-		}
-		await createBtn.click();
-		await page.getByText('Duplicated key').first().waitFor();
-		// Check that old errors are cleaned up
-		expect(await page.getByText('Key is required').count()).toEqual(0);
-		expect(await page.getByText('Duplicated key').count()).toEqual(2);
-	});
+		await test.step('Attempt to create task with types with duplicated keys', async () => {
+			await addInputTypeBtn.click();
+			await addOutputTypeBtn.click();
+			const inputs = await page.getByPlaceholder('Key').all();
+			for (const input of inputs) {
+				await input.fill('SameKey');
+			}
+			await createBtn.click();
+			await page.getByText('Duplicated key').first().waitFor();
+			// Check that old errors are cleaned up
+			expect(await page.getByText('Key is required').count()).toEqual(0);
+			expect(await page.getByText('Duplicated key').count()).toEqual(2);
+		});
 
-	await test.step('Remove type fields', async () => {
-		await page.getByLabel('Remove input type').first().click();
-		await page.getByLabel('Remove input type').click();
-		await page.getByLabel('Remove output type').first().click();
-		await page.getByLabel('Remove output type').click();
-	});
+		await test.step('Remove type fields', async () => {
+			await page.getByLabel('Remove input type').first().click();
+			await page.getByLabel('Remove input type').click();
+			await page.getByLabel('Remove output type').first().click();
+			await page.getByLabel('Remove output type').click();
+		});
 
-	await test.step('Recognize valid pydantic_v2 schema', async () => {
-		await setUploadFile(page, 'Upload non parallel args schema', pydanticV2Schema);
-		expect(page.getByRole('combobox', { name: 'Args schema version' })).toHaveValue('pydantic_v2');
-	});
+		await test.step('Recognize valid pydantic_v2 schema', async () => {
+			await setUploadFile(page, 'Upload non parallel args schema', pydanticV2Schema);
+			expect(page.getByRole('combobox', { name: 'Args schema version' })).toHaveValue(
+				'pydantic_v2'
+			);
+		});
 
-	await test.step('Recognize valid pydantic_v1 schema', async () => {
-		await setUploadFile(page, 'Upload non parallel args schema', pydanticV1Schema);
-		expect(page.getByRole('combobox', { name: 'Args schema version' })).toHaveValue('pydantic_v1');
-	});
+		await test.step('Recognize valid pydantic_v1 schema', async () => {
+			await setUploadFile(page, 'Upload non parallel args schema', pydanticV1Schema);
+			expect(page.getByRole('combobox', { name: 'Args schema version' })).toHaveValue(
+				'pydantic_v1'
+			);
+		});
 
-	await test.step('Attempt to create task with invalid JSON files', async () => {
-		await setUploadFile(page, 'Upload non parallel meta file', brokenJson);
-		await setUploadFile(page, 'Upload parallel meta file', brokenJson);
-		await setUploadFile(page, 'Upload non parallel args schema', brokenJson);
-		await setUploadFile(page, 'Upload parallel args schema', invalidArgsSchema);
-		await createBtn.click();
-		await page.getByText("File doesn't contain valid JSON").first().scrollIntoViewIfNeeded();
-		expect(
-			await page.getByText("File doesn't contain valid JSON", { exact: true }).count()
-		).toEqual(3);
-		await page
-			.getByText('File doesn\'t contain valid JSON Schema: strict mode: unknown keyword: "foo"')
-			.waitFor();
-	});
-
-	await test.step('Clear selections', async () => {
-		const clearBtn = page.getByRole('button', { name: 'Clear' }).first();
-		await clearBtn.click();
-		await clearBtn.click();
-		await clearBtn.click();
-		await clearBtn.click();
-	});
-
-	await test.step('Create a task with schema recognized as pydantic_v2 but manually set to pydantic_v1', async () => {
-		await setUploadFile(page, 'Upload non parallel args schema', validArgsSchemaFile);
-		await setUploadFile(page, 'Upload parallel args schema', validArgsSchemaFile);
-		const versionSelector = page.getByRole('combobox', { name: 'Args schema version' });
-		expect(versionSelector).toHaveValue('pydantic_v2');
-		await versionSelector.selectOption('pydantic_v1');
-		await createBtn.click();
-		await page.getByText('Task created successfully').waitFor();
-
-		const task = await getCreatedTaskModalData(page, randomTaskName4, 'compound');
-		expect(task.name).toEqual(randomTaskName4);
-		expect(task.command_non_parallel).toEqual('/tmp/test-np');
-		expect(task.command_parallel).toEqual('/tmp/test-p');
-		expect(task.args_schema_version).toEqual('pydantic_v1');
-	});
-
-	await test.step('Create different version of parallel task', async () => {
-		await page.getByRole('combobox', { name: 'Task type' }).selectOption('Parallel');
-		await page.getByRole('textbox', { name: 'Task name' }).fill(randomTaskName2);
-		await page.getByRole('textbox', { name: 'Command parallel' }).fill('/tmp/test2');
-		await page.getByRole('textbox', { name: 'Version' }).fill('0.0.2');
-		await createBtn.click();
-		await page.getByText('Task created successfully').waitFor();
-		const versionSelector = page.getByRole('row', { name: randomTaskName2 }).getByRole('combobox');
-		await expect(versionSelector).toHaveValue('0.0.2');
-		let task = await getCreatedTaskModalData(page, randomTaskName2, 'parallel');
-		expect(task.name).toEqual(randomTaskName2);
-		expect(task.command_parallel).toEqual('/tmp/test2');
-		expect(task.args_schema_version).toEqual('-');
-		await versionSelector.selectOption('0.0.1');
-		await expect(versionSelector).toHaveValue('0.0.1');
-		task = await getCreatedTaskModalData(page, randomTaskName2, 'parallel');
-		expect(task.name).toEqual(randomTaskName2);
-		expect(task.command_parallel).toEqual('/tmp/test');
-		expect(task.args_schema_version).toEqual('pydantic_v2');
-	});
-
-	await test.step('Cleanup test tasks', async () => {
-		await deleteTask(page, randomTaskName1);
-		await deleteTask(page, randomTaskName2);
-		await deleteTask(page, randomTaskName3);
-		await deleteTask(page, randomTaskName4);
+		await test.step('Attempt to create task with invalid JSON files', async () => {
+			await setUploadFile(page, 'Upload non parallel meta file', brokenJson);
+			await setUploadFile(page, 'Upload parallel meta file', brokenJson);
+			await setUploadFile(page, 'Upload non parallel args schema', brokenJson);
+			await setUploadFile(page, 'Upload parallel args schema', invalidArgsSchema);
+			await createBtn.click();
+			await page.getByText("File doesn't contain valid JSON").first().scrollIntoViewIfNeeded();
+			expect(
+				await page.getByText("File doesn't contain valid JSON", { exact: true }).count()
+			).toEqual(3);
+			await page
+				.getByText('File doesn\'t contain valid JSON Schema: strict mode: unknown keyword: "foo"')
+				.waitFor();
+		});
 	});
 });
+
+/**
+ * @param {import('@playwright/test').Page} page
+ */
+async function openSingleTaskForm(page) {
+	await page.goto('/v2/tasks/management');
+	await waitPageLoading(page);
+	await page.getByText('Single task').click();
+}
 
 /**
  * @param {import('@playwright/test').Page} page
