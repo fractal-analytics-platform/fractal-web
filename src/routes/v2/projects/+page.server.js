e6dc6357f39e2f7a19adccf7215b7f790db14322
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-05-14 15:52:54 +0200

    remove sortProjectsByTimestampCreatedDesc

--- src/routes/v2/projects/+page.server.js
+++ src/routes/v2/projects/+page.server.js
@@ -1,4 +1,3 @@
-import { sortProjectsByTimestampCreatedDesc } from '$lib/common/component_utilities';
 import { getProjectInvitations, listProjects } from '$lib/server/api/v2/project_api';
 import { getLogger } from '$lib/server/logger.js';
 
@@ -8,8 +7,6 @@ export async function load({ fetch }) {
 	logger.trace('Loading projects page');
 
 	const projects = await listProjects(fetch, true);
-	sortProjectsByTimestampCreatedDesc(projects);
-
 	const invitations = await getProjectInvitations(fetch);
 
 	return {
