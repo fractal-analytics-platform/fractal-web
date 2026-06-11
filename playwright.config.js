8c77d6d766d8d4332fafb3f445aece309a4a6f2c
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-06-11 15:08:48 +0200

    fractal-server branch

--- playwright.config.js
+++ playwright.config.js
@@ -60,7 +60,7 @@ export default defineConfig({
 
 	webServer: [
 		{
-			command: './tests/start-test-server.sh --branch main',
+			command: './tests/start-test-server.sh --branch 3163-introduce-core-tasks',
 			port: 8000,
 			waitForPort: true,
 			stdout: 'pipe',
