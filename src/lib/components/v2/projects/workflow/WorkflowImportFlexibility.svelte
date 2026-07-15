6f1d2b07592b47a84e5e5ffbc798ac52ed15cb62
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2026-07-15 12:33:12 +0200

    remove emoji, use '(core)'

--- src/lib/components/v2/projects/workflow/WorkflowImportFlexibility.svelte
+++ src/lib/components/v2/projects/workflow/WorkflowImportFlexibility.svelte
@@ -71,13 +71,8 @@
 								<option value={undefined}>Select...</option>
 								{#each [...data.available_tasks] as task, i (i)}
 									{#if !data.version || includeOlderVersions || (!includeOlderVersions && !task.older_than_target)}
-										<option
-											value={task.version}
-											title={[task.is_core ? 'Core task' : '', task.active ? '' : 'Not active']
-												.filter(Boolean)
-												.join(' - ')}
-										>
-											{task.version}{task.is_core ? ' ✅' : ''}{task.active ? '' : ' ⚠️'}
+										<option value={task.version} title={task.active ? '' : 'Not active'}>
+											{task.version}{task.is_core ? ' (core)' : ''}{task.active ? '' : ' ⚠️'}
 										</option>
 									{/if}
 								{/each}
